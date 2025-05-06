aca75ba90cf1e01f6dbaf9e9cf019455d2a1058a
AayushBade14 <aayushrajubade@gmail.com>
2025-05-06 23:04:08 +0530

    Bug fix in logger.cpp -- undefined WARN and double declaration of default argument

--- CrochetEngine/Core/Logger/Logger.cpp
+++ CrochetEngine/Core/Logger/Logger.cpp
@@ -3,22 +3,22 @@
 namespace Crochet::Core {
   
   const std::string WHITE   = "\033[0m";
-  const std::string YELLOW  = "\033[33m";
-  const std::string GREEN   = "\033[32m";
-  const std::string RED     = "\033[31m";
-  
+  const std::string YELLOW  = "\033[38;2;255;255;0m";
+  const std::string GREEN   = "\033[38;2;0;255;0m";
+  const std::string RED     = "\033[38;2;255;0;0m";
+ 
   Logger& Logger::getInstance(){
     static Logger instance;
     return instance;
   }
 
-  void Logger::log(const std::string& msg,LogLevel level = INFO){
+  void Logger::log(const std::string& msg,LogLevel){
     
     switch (level) {
       case LogLevel::INFO:
         std::cout << GREEN << "[INFO]: " << msg << WHITE << std::endl;
         break;
-      case LogLevel::WARN:
+      case LogLevel::WARNING:
         std::cout << YELLOW << "[WARNING]: " << msg << WHITE << std::endl;
         break;
       case LogLevel::ERROR:
@@ -37,7 +37,7 @@ namespace Crochet::Core {
   }
 
   void Logger::warn(const std::string& msg){
-    Logger::log(msg,LogLevel::WARN);
+    Logger::log(msg,LogLevel::WARNING);
   }
 
   void Logger::error(const std::string& msg){
