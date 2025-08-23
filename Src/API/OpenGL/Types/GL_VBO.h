c9ced1f643b4aa9e576eb625f3542cdf7c219bee
AayushBade14 <aayushrajubade@gmail.com>
2025-08-23 13:32:53 +0530

    Added OpenGLTypes::GLVBO

--- Src/API/OpenGL/Types/GL_VBO.h
+++ Src/API/OpenGL/Types/GL_VBO.h
@@ -9,15 +9,19 @@ namespace OpenGLTypes {
       std::string mName = "VBO";
 
     public:
-      GLVBO();
-      ~GLVBO();
+      GLVBO(const std::string& name);
+      ~GLVBO() = default;
       
       const GLBuffer& GetBufferObject() const {return mVbo;}
       const std::string& GetName() const {return mName;}
+      GLenum GetTarget() {return mVbo.GetTarget();}
+      const GLuint& GetId() const {return mVbo.GetId();}
 
       void Bind();
       void Unbind();
       
-      
+      void UpdateVbo(GLsizeiptr size, GLenum usage, const void* data);
+      void UpdateVboPartial(GLintptr offset, GLsizeiptr size, const void* data);
+      void UpdateVboNoFill(GLsizeiptr size, GLenum usage);
   };
 }
