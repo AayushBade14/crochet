0f1e4eea34c8f3aa0c6b633b27d367ddd8f4f42e
AayushBade14 <aayushrajubade@gmail.com>
2026-01-16 00:16:19 +0530

    Added App/Application.h: Header for the application class

---
+++ App/Application.h
@@ -0,0 +1,20 @@
+//-------------------------------------------------------------------------
+//  Name: Application.h
+//
+//  Desc: Header for Application
+//
+//  Author: Aayush Bade 2026 (aayushbade14.github.io/Portfolio)
+//-------------------------------------------------------------------------
+
+#pragma once
+
+namespace Crochet
+{
+  class Application
+  {
+    public:
+
+      void Run();
+  };
+}
+
