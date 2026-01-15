9011a7bacbeb07ea471b5fa297a65c1f6000877d
AayushBade14 <aayushrajubade@gmail.com>
2026-01-15 22:54:42 +0530

    Added Core/Window/WindowProp.h : Contains the struct declaration for holding window-properties

---
+++ Core/Window/WindowProp.h
@@ -0,0 +1,22 @@
+//-------------------------------------------------------------------------
+//  Name: WindowProp.h
+//
+//  Desc: A structure for holding window-specific properties
+//
+//  Author: Aayush Bade 2026 (aayushbade14.github.io/Portfolio)
+//-------------------------------------------------------------------------
+
+#pragma once
+
+#include <string>
+
+namespace Crochet
+{
+  struct WindowProp
+  {
+    int Width;
+    int Height;
+    std::string Title;
+  };
+}
+
