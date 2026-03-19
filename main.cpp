92765da88969bec782d8517c011594b277020adf
AayushBade14 <aayushrajubade@gmail.com>
2026-03-19 15:30:35 +0530

    Added main.cpp: Simple window program

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <cstdint>
+
+int Width = 1920;
+int Height = 1013;
+std::string Title = "crochet-editor";
+
+GLFWwindow* window = nullptr;
+
+int main(void)
+{ 
+  
+  glfwInit();
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  window = glfwCreateWindow(Width,
+                            Height,
+                            Title,
+                            nullptr,
+                            nullptr);
+
+  glfwMakeContextCurrent(window);
+
+  gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+  while(!glfwWindowShouldClose(window))
+  {
+    glfwPollEvents();
+
+    glClearColor(0.0, 0.0, 1.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+  
+    glfwSwapBuffers(window);
+  }
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
+
+  return 0;
+}
